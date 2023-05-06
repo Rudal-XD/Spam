a6ebea0313b35fea99d3f4855386794c6099ec0a
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-05-06 12:22:19 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ import sys
 
 def main():
 	os.system('clear')
-	os.system('figlet papiking21')
+	os.system('figlet Rudal-XD')
 	banner='''
 
-	[+]AUTHOR:Papi king21
-	[+]Youtube : Papi King21
+	[+]AUTHOR  :Rudal-XD
+	[+]Youtube : -
 	'''
 	print(banner)
 	no = input(' target : ')
